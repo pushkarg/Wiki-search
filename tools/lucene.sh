353b93f22365e1e12be4f950ed1eb9acfd0668bd
Pushkar Gopalakrishna <pushkar87_dg@yahoo.co.in>
2012-02-12 17:10:27 -0800

    Added the script to run WikiSearcher for the 30 queries

---
+++ tools/lucene.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+CP=".:/home/pushkar/program/project/Wiki-Search/Jars/jdom-1.1.2.jar:/home/pushkar/program/project/Wiki-Search/Jars/lucene-core-3.5.0.jar:/home/pushkar/program/project/Wiki-Search/Wiki-search/lib:/home/pushkar/program/project/Wiki-Search/Jars/vtd-xml.jar"
+java -cp $CP WikiSearcher "DNA"	> done/lucene1.txt
+java -cp $CP WikiSearcher "Apple"	> done/lucene2.txt
+java -cp $CP WikiSearcher "Epigenetics"	> done/lucene3.txt
+java -cp $CP WikiSearcher "Hollywood"	> done/lucene4.txt
+java -cp $CP WikiSearcher "Maya"	> done/lucene5.txt
+java -cp $CP WikiSearcher "Microsoft"	> done/lucene6.txt
+java -cp $CP WikiSearcher "Precision"	> done/lucene7.txt
+java -cp $CP WikiSearcher "Tuscany"	> done/lucene8.txt
+java -cp $CP WikiSearcher "99 balloons"	> done/lucene9.txt
+java -cp $CP WikiSearcher "Computer Programming"	> done/lucene10.txt
+java -cp $CP WikiSearcher "Financial meltdown"	> done/lucene11.txt
+java -cp $CP WikiSearcher "Justin Timberlake"	> done/lucene12.txt
+java -cp $CP WikiSearcher "Least Squares"	> done/lucene13.txt
+java -cp $CP WikiSearcher "Mars robots"	> done/lucene14.txt
+java -cp $CP WikiSearcher "Page six"	> done/lucene15.txt
+java -cp $CP WikiSearcher "Roman Empire"	> done/lucene16.txt
+java -cp $CP WikiSearcher "Solar energy"	> done/lucene17.txt
+java -cp $CP WikiSearcher "Statistical Significance"	> done/lucene18.txt
+java -cp $CP WikiSearcher "Steve Jobs"	> done/lucene19.txt
+java -cp $CP WikiSearcher "The Maya"	> done/lucene20.txt
+java -cp $CP WikiSearcher "Triple Cross"	> done/lucene21.txt
+java -cp $CP WikiSearcher "US Constitution"	> done/lucene22.txt
+java -cp $CP WikiSearcher "Eye of Horus"	> done/lucene23.txt
+java -cp $CP WikiSearcher "Madam I’m Adam"	> done/lucene24.txt
+java -cp $CP WikiSearcher "Mean Average Precision"	> done/lucene25.txt
+java -cp $CP WikiSearcher "Physics Nobel Prizes"	> done/lucene26.txt
+java -cp $CP WikiSearcher "Read the manual"	> done/lucene27.txt
+java -cp $CP WikiSearcher "Spanish Civil War"	> done/lucene28.txt
+java -cp $CP WikiSearcher "Do geese see god"	> done/lucene29.txt
+java -cp $CP WikiSearcher "Much ado about nothing"	> done/lucene30
